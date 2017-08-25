7453168d5c805a1c0c2fd08247bf7fe9fcee5f14
wellmotta <wellingthonmotta@hotmail.com>
2017-08-25 03:27:55 -0300

    Create contar-caracteres.py
    create contar-caracteres.py

---
+++ Python/contar-caracteres.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+# Pergunta: https://pt.stackoverflow.com/questions/232172
+
+# Informe uma frase
+frase = input("Informe uma frase: ")
+# Informe qual o caractere que deseja saber a quantidade contida na frase
+caracter = input("Informe o caracter que deseja contar: ")
+# método: count - Conta quantas vezes o caractere foi usado na frase
+# Imprime a quantidade.
+print(frase.count(caracter))
