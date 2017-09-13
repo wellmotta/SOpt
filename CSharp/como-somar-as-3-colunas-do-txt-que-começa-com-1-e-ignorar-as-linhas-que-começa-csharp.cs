cdd1437a290e5fe859f333b2889b9f065ca1f0a5
wellmotta <wellingthonmotta@hotmail.com>
2017-09-12 22:23:27 -0300

    Create como-somar-as-3-colunas-do-txt-que-começa-com-1-e-ignorar-as-linhas-que-começa-csharp.cs

---
+++ CSharp/como-somar-as-3-colunas-do-txt-que-começa-com-1-e-ignorar-as-linhas-que-começa-csharp.cs
@@ -0,0 +1,23 @@
+// https://pt.stackoverflow.com/questions/236832
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		long number = 0;
+		// Dados do txt
+		string dados = "020175646464654647658785362342234605143545654 10432434545 16456676577 12344565467 954554646467657776577673";
+		// Remove os espaços guardando em Array
+		string[] arr = dados.Split(null);
+		for (int i = 1; i < arr.Length; i++) {
+			// Verifica se o primeiro digito é o numero 1
+			// Se for faz a soma
+			if (arr[i][0].ToString() == "1") {
+				number += Convert.ToInt64(arr[i]);
+			}
+		}
+		Console.WriteLine(number);
+	}
+}
