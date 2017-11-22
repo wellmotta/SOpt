3c3d88cf5d50a36a09e56819c2f690c6eab24f2d
wellmotta <wellingthonmotta@hotmail.com>
2017-11-22 00:28:33 -0200

    Update serial.class.php

--- PHP/gerar-serial-key-com-prefixo/serial.class.php
+++ PHP/gerar-serial-key-com-prefixo/serial.class.php
@@ -7,30 +7,31 @@
 class Serial
 {
   private $prefixo = "";
-  private $max = 0;
-  public $chars = array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-
+  public $letras = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+  public $numeros = array(0,1,2,3,4,5,6,7,8,9);
+  
   /* 
    * Método construtor
    * Cria um prefixo para cada objeto criado
    */
   public function __construct() {
-    $this->max = count($this->chars)-1;
+    $max = count($this->letras)-1;
     for($a=0;$a<5;$a++) {
-      $this->prefixo .= $this->chars[rand(0, $this->max)];
+      $this->prefixo .= $this->letras[rand(0, $max)];
     }
   }
-
+    
   /* 
-   * Método getSerial
+   * Método get
    * Gera um serial de 15 digitos toda vez que o método for chamado
    */
-  public function getSerial() {
+  public function get() {
     $serial = '';
+    $max = count($this->numeros)-1;
     for($b=0;$b<15;$b++){
-      $serial .= (!($b % 5) && $b ? '-' : ''). $this->chars[rand(0, $this->max)];
+      $serial .= (!($b % 5) && $b ? '-' : ''). $this->numeros[rand(0, $max)];
     }
     return $this->prefixo."-".$serial;
   }
-
+  
 }
